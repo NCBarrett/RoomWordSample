bddf2e3090ba0411272a72eb779b78e86ce28ec3
NCBarrett <80253863+NCBarrett@users.noreply.github.com>
2022-07-20 11:21:21 -0700

    Adding NewWordActivity

---
+++ app/src/main/java/com/neillbarrett/roomwordsample/NewWordActivity.kt
@@ -0,0 +1,25 @@
+package com.neillbarrett.roomwordsample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+
+class NewWordActivity : AppCompatActivity() {
+
+    private lateinit var editWordView: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+        editWordView = findViewById(R.id.edit_word)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            startActivity(replyIntent)
+        }
+    }
+}
