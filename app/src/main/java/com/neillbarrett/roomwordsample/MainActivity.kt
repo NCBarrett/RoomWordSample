8393440e0eeb9acb0e60a4649b0dd79479b27b5e
NCBarrett <80253863+NCBarrett@users.noreply.github.com>
2022-07-21 11:28:39 -0700

    Finished Roow with a View

--- app/src/main/java/com/neillbarrett/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/neillbarrett/roomwordsample/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.neillbarrett.roomwordsample
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                    val word = Word(0, it)
+                    wordViewModel.insert(word)
+                }
+            } else {
+                Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_LONG).show()
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +39,17 @@ class MainActivity : AppCompatActivity() {
         val adapter = WordListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.submitList(it) }
+        })
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, NewWordActivity::class.java)
+            resultLauncher.launch(intent)
+        }
     }
 }
+
--- app/src/main/java/com/neillbarrett/roomwordsample/NewWordActivity.kt
+++ app/src/main/java/com/neillbarrett/roomwordsample/NewWordActivity.kt
@@ -1,5 +1,6 @@
 package com.neillbarrett.roomwordsample
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,7 +20,18 @@ class NewWordActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.button_save)
         button.setOnClickListener {
             val replyIntent = Intent()
-            startActivity(replyIntent)
+            if (TextUtils.isEmpty(editWordView.text)){
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editWordView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
         }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
     }
 }
